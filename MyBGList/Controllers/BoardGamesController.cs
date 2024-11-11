8d1d2982233deadbc335532922a30cbd04fa40a0
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-11-11 07:14:46 -0600

    HATEOAS Implementation

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -16,30 +16,43 @@ namespace MyBGList.Controllers
         }
 
         [HttpGet(Name = "GetBoardGames")]
-        public IEnumerable<BoardGame> Get()
+        public RestDTO<BoardGame[]> Get() 
         {
-            return new[] {
-                new BoardGame() {
-                    Id = 1,
-                    Name = "Axis & Allies",
-                    Year = 1981,
-                    MinPlayers = 2,
-                    MaxPlayers = 5
+            return new RestDTO<BoardGame[]>()
+            {
+
+                Data = new[] 
+                {
+                    new BoardGame() {
+                        Id = 1,
+                        Name = "Axis & Allies",
+                        Year = 1981
+                    },
+
+                    new BoardGame() {
+                        Id = 2,
+                        Name = "Citadels",
+                        Year = 2000
+                    },
+
+                    new BoardGame() {
+                        Id = 3,
+                        Name = "Terraforming Mars",
+                        Year = 2016
+                    }
                 },
-                new BoardGame() {
-                    Id = 2,
-                    Name = "Citadels",
-                    Year = 2000,
-                    MinPlayers = 2,
-                    MaxPlayers = 8
-                },
-                new BoardGame() {
-                    Id = 3,
-                    Name = "Terraforming Mars",
-                    Year = 2016,
-                    MinPlayers = 1,
-                    MaxPlayers = 5
+
+
+                Links = new List<LinkDTO> 
+                {
+                    new LinkDTO
+                    (
+                        Url.Action(null, "BoardGames", null, Request.Scheme)!,
+                        "self",
+                        "GET"
+                    ),
                 }
+
             };
         }
 
---
+++ MyBGList/DTO/LinkDTO.cs
@@ -0,0 +1,25 @@
+﻿
+/*
+ * This Class will host Descriptive Links
+    1. This LinkDTO class is a Data Transfer Object (DTO) designed to encapsulate information about a hyperlink.
+    2. Links are included in API responses to provide additional context or navigational options to the client.
+*/
+
+namespace MyBGList.DTO
+{
+    public class LinkDTO
+    {
+        public LinkDTO(string href, string rel, string type)
+        {
+            Href = href;
+            Rel = rel;
+            Type = type;
+        }
+
+        public string Href { get; private set; }
+
+        public string Rel { get; private set; }
+
+        public string Type { get; private set; }
+    }
+}
